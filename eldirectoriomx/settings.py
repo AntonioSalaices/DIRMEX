829aac2e79e766eeb246c32fe69bf8e60c3175dd
Antonio Salaices Monzón <antonio.salaicesm@gmail.com>
2019-12-18 05:34:44 -0700

    cambios bd2

--- eldirectoriomx/settings.py
+++ eldirectoriomx/settings.py
@@ -103,24 +103,24 @@ REST_FRAMEWORK = {
 #         'PORT': '3306',
 #     }
 # }
-if DEBUG:
-    DATABASES = {
+# if DEBUG:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dirmex',
+        'USER': 'salaices',
+        'PASSWORD': 'pass123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'dirmex',
-            'USER': 'salaices',
-            'PASSWORD': 'pass123',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
